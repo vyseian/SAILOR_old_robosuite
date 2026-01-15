2fc9eb5273095d02536e912362dbcbdef853f684
Ian Vyse <ian.vyse@mail.utoronto.ca>
2026-01-14 20:28:11 -0500

    Fixed SAILOR code and config

--- datasets/download_datasets.py
+++ datasets/download_datasets.py
@@ -11,3 +11,6 @@ for file in files:
     if file not in ["README.md", ".gitignore"]:
         print(f"Downloading {file}...")
         hf_hub_download(repo_id=repo_id, repo_type=repo_type, filename=file, local_dir=".", local_dir_use_symlinks=False)
+
+# http://downloads.cs.stanford.edu/downloads/rt_benchmark/transport/mh/demo_v141.hdf5
+# http://downloads.cs.stanford.edu/downloads/rt_benchmark/square/mh/demo_v141.hdf5
